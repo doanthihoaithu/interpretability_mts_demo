9c450de0ab93652ae294cae0ba699974d31cca43
Thu T. H. Doan <hoaithuxt1998@gmail.com>
2025-12-23 08:16:55 +0100

    Update Overview.py

--- pages/Overview.py
+++ pages/Overview.py
@@ -4,6 +4,10 @@ from PIL import Image
 st.markdown('# Overview')
 st.markdown('The following figures provide a visual summary of the average VUS-PR and AUC-PR, along with interpretability metrics (Hit2 and KL divergence), across different components.')
 st.markdown('These components include: (i) eight individual multivariate detectors; (ii) the Oracle (shown in white), representing the theoretical upper bound of model selection performance; (iii) Averaging Ensembling (shown in orange), which combines detector outputs via simple averaging; and (iv) Best Time Series Classification (shown in blue), corresponding to the best-performing multivariate time series classification model used for model selection.')
+st.markdown('Click tab Interpretability for interactive visualization')
+
+
+
 image_path = "figures/AUC_PR.png"
 image = Image.open(image_path)
 st.image(image, caption='AUC_PR of detectors on synthetic datasets')
