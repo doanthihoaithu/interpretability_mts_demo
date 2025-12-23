4839ead1afc5f5f74a4962acdb3eef5c33aa6c18
Thu T. H. Doan <hoaithuxt1998@gmail.com>
2025-12-23 08:12:39 +0100

    Update Overview.py

--- pages/Overview.py
+++ pages/Overview.py
@@ -2,8 +2,8 @@ import streamlit as st
 from PIL import Image
 
 st.markdown('# Overview')
-# st.markdown('The following figures provide a visual summary of the average VUS-PR and AUC-PR, along with interpretability metrics (Hit2 and KL divergence), across different components. These components include: (i) eight individual multivariate detectors; (ii) the Oracle (shown in white), representing the theoretical upper bound of model selection performance; (iii) Averaging Ensembling (shown in orange), which combines detector outputs via simple averaging; and (iv) Best Time Series Classification (shown in blue), corresponding to the best-performing multivariate time series classification model used for model selection.'
-
+st.markdown('The following figures provide a visual summary of the average VUS-PR and AUC-PR, along with interpretability metrics (Hit2 and KL divergence), across different components.')
+st.markdown('These components include: (i) eight individual multivariate detectors; (ii) the Oracle (shown in white), representing the theoretical upper bound of model selection performance; (iii) Averaging Ensembling (shown in orange), which combines detector outputs via simple averaging; and (iv) Best Time Series Classification (shown in blue), corresponding to the best-performing multivariate time series classification model used for model selection.')
 image_path = "figures/AUC_PR.png"
 image = Image.open(image_path)
 st.image(image, caption='AUC_PR of detectors on synthetic datasets')
